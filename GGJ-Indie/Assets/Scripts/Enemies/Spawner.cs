caaa2c3db19881c2f5ac39203f9d81604889185d
mazafrav <mazafrav@gmail.com>
2024-01-28 12:59:17 +0100

    update spawner

--- GGJ-Indie/Assets/Scripts/Enemies/Spawner.cs
+++ GGJ-Indie/Assets/Scripts/Enemies/Spawner.cs
@@ -31,6 +31,10 @@ public class Spawner : MonoBehaviour
         playerProgress = gameManager.GetComponent<GameManager>().GetProgression();
         previousProgress = gameManager.GetComponent<GameManager>().GetProgression();
         maxSpawnRate = spawnRate;
+        enemies.Add(normal);
+        enemies.Add(depression);
+        enemies.Add(ranged);
+        enemies.Add(anxiety);
         bCanSpawn = true;
     }
 
@@ -54,15 +58,14 @@ public class Spawner : MonoBehaviour
 
                     previousProgress = playerProgress;
                }
-         
-                Vector3 v = player.transform.up;
-                float angle = Random.Range(0, 360.0f);//- (player.transform.position + direction).x;
+                
+                float randomX = Random.Range(-7.7f, 7.7f);//- (player.transform.position + direction).x;
+                float randomY = Random.Range(-4.0f, 4.0f); //- (player.transform.position + direction).y;
+                Vector3 posToSpawn = new Vector3(randomX * radius, randomY + radius, 0.0f);
+                Instantiate(enemy, posToSpawn, Quaternion.identity);
+               
+                //Debug.DrawLine(player.transform.position + direction, posToSpawn, Color.red, Time.deltaTime, false);
 
-                v = Quaternion.Euler(0, 0, angle) * v;
-                Vector3 spawnPos = (player.transform.position + v) * Random.Range(radius, 2*radius);
-
-                Debug.DrawLine(player.transform.position, spawnPos, Color.red, 10, false);
-                Instantiate(enemy, spawnPos, Quaternion.identity);
                 StartCoroutine(ReloadSpawn());
             }
         }
@@ -80,6 +83,7 @@ public class Spawner : MonoBehaviour
         int index = 0;
         int rndMax = 0;
         int rndMin = 0;
+        float playerProgress = gameManager.GetComponent<GameManager>().GetProgression();
         if (playerProgress > 20.0f)
         {
             rndMax = 1;
@@ -97,7 +101,10 @@ public class Spawner : MonoBehaviour
             rndMin = 1;
             rndMax = 3;
         }
-        index = Random.Range(rndMin, rndMax);
+        if (rndMax != 0)
+        {
+            index = Random.Range(rndMin, rndMax);
+        }
         GameObject enemyToSpawn = enemies[index];
         return enemyToSpawn;
     }
